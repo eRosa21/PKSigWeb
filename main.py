c3181e9221aef6360d2739ff943edca9c74f97ae
Esthéfano <rosaesthefano@gmail.com>
2026-07-22 09:49:49 -0300

    Update the main code

--- main.py
+++ main.py
@@ -3,25 +3,19 @@ from fastapi import FastAPI, Depends, HTTPException
 # pyrefly: ignore [missing-import]
 from sqlalchemy.orm import Session
 from database import engine, Base, get_db
-from models import Region, Route, City
+from models import Region, Route, City, Pokemon 
 import requests
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+## Regions ===================================================
+
 @app.get("/regions")
 def get_regions(db: Session = Depends(get_db)):
     regions = db.query(Region).all()
     return regions
-
-#@app.post("/regions/create")
-#def create_region(name: str, generation: int,  db: Session = Depends(get_db)):
- #   region = Region(name=name, generation=generation)
-  #  db.add(region)
-   # db.commit()
-    #db.refresh(region)
-    #return region
 
 @app.post("/region/add")
 def add_region(name:str,generation:int,db:Session = Depends(get_db)):    
@@ -57,13 +51,14 @@ def delete_region(region_id:int, name:str, generation:int, db:Session = Depends(
     db.commit()
     return {"Message": f"A Região {region.name} deletada com sucesso"}
 
+## ROUTES ===================================================
+
+
 @app.post("/routes/add")
-def add_route(region_id: int, route_number: int, 
-#lat: float, lng: float, 
+def add_route(region_id: int, route_number: int,pokeapi_name:str, lat: float, lng: float, 
 db: Session = Depends(get_db)):
-    route = Route(region_id=region_id, route_number=route_number,
-    # lat=lat, lng=lng
-    )
+    route = Route(region_id=region_id, route_number=route_number,pokeapi_name=pokeapi_name,
+    lat=lat, lng=lng)
     db.add(route)
     db.commit()
     db.refresh(route)
@@ -74,13 +69,13 @@ def get_routes(db: Session = Depends(get_db)):
     routes = db.query(Route).all()
     return routes
 
+## CITIES ===================================================
+
 @app.post("/cities/add")
-def add_city(region_id: int, name: str,
-#lat: float, lng: float,
+def add_city(region_id: int, name: str,pokeapi_name:str, lat: float, lng: float,
 description: str, gym: str, gym_type:str,db: Session = Depends(get_db)):
-    city = City(region_id=region_id, name=name,
-    #lng = lng, lat = lat,
-    description = description, gym = gym, gym_type = gym_type)
+    city = City(region_id=region_id, name=name,pokeapi_name=pokeapi_name,
+    lng = lng, lat = lat, description = description, gym = gym, gym_type = gym_type)
     db.add(city)
     db.commit()
     db.refresh(city)
@@ -91,28 +86,20 @@ def get_cities(db: Session = Depends(get_db)):
     cities = db.query(City).all()
     return cities
 
+## POKEMON ===================================================
+
 @app.get("/pokemon/{pokemon_identifier}")
-def get_pokemon_locations(pokemon_identifier: str):
+def get_pokemon_locations(pokemon_identifier: str,db: Session = Depends(get_db)):
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_identifier.lower()}/encounters"
     resposta = requests.get(url)
 
-    if resposta.status_code == 200:
-        info = resposta.json()
-        locations = [item['location_area']['name'] for item in info]
-        return locations
-    else:
-        return []
+    if resposta.status_code != 200:
+        raise HTTPException(status_code=404,detail ="Pokemon não encontrado ou erro na API")
+    
+    info = resposta.json()
+    location_names = [item['location_area']['name'] for item in info]
 
-pokemon_name = "turtwig"
-pokemon_id = "387"
-pokemon_info = get_pokemon_locations(pokemon_id or pokemon_name)
-
-if pokemon_info:
-    print(f"Localizações da espécie {pokemon_name}:")
-    for loc in pokemon_info:
-        print(f'- {loc}')
-else:
-    print(f"Não foi possivel encontrar informações de {pokemon_name}")
+    return {"pokemon": pokemon_identifier, "pokeapi_locations": location_names}
 
 if __name__ == "__main__":
     # pyrefly: ignore [missing-import]
--- models.py
+++ models.py
@@ -16,17 +16,30 @@ class Route(Base):
     id = Column(Integer, primary_key=True, index=True)
     region_id = Column(Integer, ForeignKey("regions.id"), nullable=False)
     route_number = Column(Integer, nullable=False)
-    #lat = Column(Float, nullable=True)
-    #lng = Column(Float, nullable=True)
+    pokeapi_name = Column(String(250), nullable = False)
+    lat = Column(Float, nullable=True)
+    lng = Column(Float, nullable=True)
 
 class City(Base):
     __tablename__ = "cities"
     
     id = Column(Integer, primary_key=True, index=True)
     region_id = Column(Integer, ForeignKey("regions.id"), nullable=False)
-    name = Column(String(100), nullable=False)
-    #lat = Column(Float, nullable=True)
-    #lng = Column(Float, nullable=True)
+    pokeapi_name = Column(String(250), nullable = False)
+    lat = Column(Float, nullable=True)
+    lng = Column(Float, nullable=True)
     gym = Column(String(100))
     gym_type = Column(String(20))
     description = Column(Text)
+
+class Pokemon(Base):
+    __tablename__ = "pokemon"
+
+    id = Column(Integer,primary_key=True)
+    name = Column(String(50),unique= True, nullable=False)
+    dex_number = Column(Integer,unique =True, nullable=False)
+    type1 = Column(String(20), nullable=False)
+    type2 = Column(String(20))
+    weight = Column(Integer, nullable=False)
+    height = Column(Integer, nullable=False)
+

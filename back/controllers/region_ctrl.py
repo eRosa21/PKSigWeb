c7193ee811ca0f09ba3a1981aa6e3e5931784f46
Esthéfano <rosaesthefano@gmail.com>
2026-07-28 09:36:22 -0300

    style: removed comments in the routes

--- back/controllers/region_ctrl.py
+++ back/controllers/region_ctrl.py
@@ -6,5 +6,4 @@ region_router = APIRouter(prefix="/region", tags=["Region"])
 
 @region_router.get("/get")
 async def get_regions(db: Session = Depends(get_db)):
-    # Aqui você pode adicionar a lógica para buscar as regiões no banco de dados
     return {"message": "Lista de regiões retornada com sucesso"}
--- back/controllers/route_ctrl.py
+++ back/controllers/route_ctrl.py
@@ -6,5 +6,4 @@ route_router = APIRouter(prefix="/route", tags=["Route"])
 
 @route_router.get("/get")
 async def get_routes(db: Session = Depends(get_db)):
-    # Aqui você pode adicionar a lógica para buscar as rotas no banco de dados
     return {"message": "Lista de rotas retornada com sucesso"}

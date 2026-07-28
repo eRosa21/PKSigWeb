d5e17fe088c63e6d9f3c5d1af89daff44386e174
Esthéfano <rosaesthefano@gmail.com>
2026-07-28 09:14:36 -0300

    refactor: get router updated

--- back/controllers/pkm_ctrl.py
+++ back/controllers/pkm_ctrl.py
@@ -4,6 +4,10 @@ from main import get_db
 
 pokemon_router = APIRouter(prefix="/pkm", tags=["Pokemon"])
 
-@pokemon_router.get("/add")
-async def add_pokemon(name: str, type1: str, type2: str = None, db: Session = Depends(get_db)):
-    return {"Conectado"}
+@pokemon_router.post("/add")
+async def add_pokemon(name: str, number: int, type1: str, type2: str = None, db: Session = Depends(get_db)):
+    return {"Informações dos Pokémon atualizadas e adicionados"}
+
+@pokemon_router.get("/get")
+async def get_pokemon(name: str, number: int, db: Session = Depends(get_db)):
+    return {"Informações do Pokémon retornadas com sucesso"}

c68b9be26d07f4ad0ef0c094e4d2f6e3448c37ef
Esthéfano <rosaesthefano@gmail.com>
2026-07-29 09:40:51 -0300

    refactor: test if pycache its ignored

--- back/controllers/city_ctrl.py
+++ back/controllers/city_ctrl.py
@@ -6,4 +6,6 @@ cities_router = APIRouter(prefix="/cities", tags=["Cities"])
 
 @cities_router.get("/get")
 async def get_cities(db: Session = Depends(get_db)):
+    
+    raise HTTPException(status_code=200, detail="Lista de cidades retornada com sucesso")
     return {"message": "Lista de cidades retornada com sucesso"}
